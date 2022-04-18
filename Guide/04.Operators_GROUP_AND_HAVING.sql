06f1fbf2f944a3e00626e86ad685fc70e4eaaaab
Anatolich <anatolich.work.english@gmail.com>
2022-04-19 01:28:14 +0300

    Add Guide/04.Operators_GROUP_AND_HAVING

---
+++ Guide/04.Operators_GROUP_AND_HAVING.sql
@@ -0,0 +1,18 @@
+-- # Grouping and sorting
+-- Count the number of students in each class and also sort them in descending order of the number of students. 
+-- You can get the student's belonging to a specific class from the Student_in_class table. 
+-- As a result, display the class identifier (class field) and the number of students in this class.
+-- Use the count alias to display the number of students.
+SELECT class, COUNT(student) AS count FROM Student_in_class GROUP BY class ORDER BY count DESC;
+
+-- # Aggregation functions MIN and MAX
+-- Find the oldest family members (use birthday field) among all existing families based on their status (status field). 
+-- Display the status and date of birth.
+-- Use the birthday alias to display the date of birth.
+SELECT status, MIN(birthday) as birthday FROM FamilyMembers GROUP BY status ORDER BY birthday DESC; 
+
+-- # Aggregation function AVG
+-- Get the average flight time for each aircraft model. 
+-- Output the plane field and the average flight time in seconds.
+-- Use the time alias to display the time.
+-- Use TIMESTAMPDIFF(second, time_out, time_in) function to get the time difference in seconds between two dates.
