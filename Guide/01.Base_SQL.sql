113149b4e8b98dc3c05aa82711f478eb220f2dd0
Anatolich <anatolich.work.english@gmail.com>
2022-04-19 01:04:26 +0300

    Update Guide/01.Base_SQl

--- Guide/01.Base_SQL.sql
+++ Guide/01.Base_SQL.sql
@@ -1,13 +1,16 @@
+-- # Selecting by single column
 -- Pick name field from Passenger table.
 SELECT name FROM Passenger;
 
+-- # Selecting by multiple columns
 -- Pick member_id, member_name and status fields from FamilyMembers table.
 SELECT member_id, member_name, status FROM FamilyMembers;
 
+-- # Selecting all columns
 -- Select all columns from Payments table.
 SELECT * FROM Payments;
 
--- Complete the request below to see the list of passengers. As you can see Bruce Willis was here twice (at the beginning and at the end of the list).
+-- # Selecting unique rows
+-- Complete the request below to see the list of passengers. 
 -- Your task is to get a list of unique passenger names by modifying the proposed request.
-
 SELECT DISTINCT name FROM Passenger;
